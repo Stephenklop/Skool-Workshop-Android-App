35f4753c060e78ac45bd03a660374263706018bf
FSGabrsek <f.gabrsek@gmail.com>
2021-05-26 11:02:42 +0200

    added dashboard item card

--- app/src/main/java/com/example/skoolworkshop2/MainActivity.java
+++ app/src/main/java/com/example/skoolworkshop2/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.skoolworkshop2;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -18,7 +21,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ImageView mWorkshopCardImg = findViewById(R.id.img_workshop_card);
-        mWorkshopCardImg.setClipToOutline(true);
+        RelativeLayout mDashItem = findViewById(R.id.item_dashboard);
+        TextView mDashTv = mDashItem.findViewById(R.id.tv_dashboard);
+        mDashTv.setText("test");
     }
 }
