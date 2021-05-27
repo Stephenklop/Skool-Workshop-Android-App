c2d2769d88d839532e7cc4338e5a8e35bc53862d
Bas <bbuijsen@gmail.com>
2021-05-27 11:25:06 +0200

    fixed nullpointerexception and started work on menu

--- app/src/main/java/com/example/skoolworkshop2/MainActivity.java
+++ app/src/main/java/com/example/skoolworkshop2/MainActivity.java
@@ -1,43 +1,41 @@
 package com.example.skoolworkshop2;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import com.google.android.material.bottomnavigation.BottomNavigationItemView;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    LinearLayout tabsll = findViewById(R.id.tabs);
-    TextView tv1 = tabsll.findViewById(R.id.a);
-    TextView tv2= tabsll.findViewById(R.id.b);
-    TextView tv3 = tabsll.findViewById(R.id.c);
-    TextView tv4 = tabsll.findViewById(R.id.d);
+public class MainActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_workshop_details);
+        setContentView(R.layout.activity_home);
 
 
+        View menu = (View) findViewById(R.id.activity_home_menu_buttons);
+        menu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
-        tv1.setSelected(true);
+            }
+        });
 
     }
 
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.b:
-            tv2.setSelected(true);
-        }
-    }
 }
