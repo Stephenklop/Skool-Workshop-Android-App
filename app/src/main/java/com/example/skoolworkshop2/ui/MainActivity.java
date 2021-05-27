2ef6e38e734f2a9552124035c370fd447e540a81
Bas <bbuijsen@gmail.com>
2021-05-27 12:07:28 +0200

    added home page

---
+++ app/src/main/java/com/example/skoolworkshop2/ui/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.skoolworkshop2.ui;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.skoolworkshop2.R;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+
+    }
+
+}
