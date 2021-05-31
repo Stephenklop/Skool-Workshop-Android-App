36da17201652fae4d6c2cd064b58692c4d3210cf
Enes <enes123454@hotmail.com>
2021-05-31 11:43:53 +0200

    Validation wordt nu op een andere manier gemaakt, dit was eerst met abstract class

---
+++ app/src/test/java/com/example/skoolworkshop2/EmailValidatorTest.java
@@ -0,0 +1,13 @@
+package com.example.skoolworkshop2;
+
+
+import org.junit.Test;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class EmailValidatorTest {
+
+    @Test
+    public void personValidator_CorrectPersonsGiven_ReturnsTrue() {
+    }
+}
