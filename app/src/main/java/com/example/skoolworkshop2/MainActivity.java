a8423ce6b97fbff2d96017916f5fc8318e2c4ab5
FSGabrsek <f.gabrsek@gmail.com>
2021-05-31 11:03:10 +0200

    temp commit

--- app/src/main/java/com/example/skoolworkshop2/MainActivity.java
+++ app/src/main/java/com/example/skoolworkshop2/MainActivity.java
@@ -11,13 +11,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_cultureday_booking);
-
-        Spinner mCategorySpnr = findViewById(R.id.activity_cultureday_booking_spnr_category);
-        mCategorySpnr.setAdapter(new WorkshopArrayAdapter(this, new String[]{"Beeldende kunst", "Theater"}));
-
-        Spinner mWorkshopSpnr = findViewById(R.id.activity_cultureday_booking_spnr_workshop);
-        mWorkshopSpnr.setAdapter(new WorkshopArrayAdapter(this, new String[]{"workshop", "workshop", "workshop", "workshop", "workshop"}));
+        setContentView(R.layout.activity_home);
 
     }
 }
