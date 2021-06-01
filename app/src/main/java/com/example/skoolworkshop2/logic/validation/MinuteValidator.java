74f2ccab2b4912fc0310292b55ffd758bbd7b65a
Enes <enes123454@hotmail.com>
2021-06-01 13:59:53 +0200

    Implementation for workshop booking (bijna klaar, fairys maakt de rest)

--- app/src/main/java/com/example/skoolworkshop2/logic/validation/MinuteValidator.java
+++ app/src/main/java/com/example/skoolworkshop2/logic/validation/MinuteValidator.java
@@ -3,9 +3,8 @@ import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
 
-public class MinuteValidator implements TextWatcher{
+public class MinuteValidator{
 
-    private String LOG_TAG = getClass().getSimpleName();
     private boolean mIsValid = false;
 
     public boolean isValid() {
@@ -13,29 +12,12 @@ public class MinuteValidator implements TextWatcher{
     }
 
     public static boolean isValidMinute(CharSequence minutes) {
-        int minute = Integer.valueOf(minutes.toString());
-        return minute <= 120 && minute >= 45;
-
-    }
-    @Override
-    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-    }
-
-    @Override
-    public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-    }
-
-    @Override
-    public void afterTextChanged(Editable s) {
-        if (s.length() != 0 ) {
-            mIsValid = isValidMinute(s);
-            if (mIsValid == true) {
-                Log.d(LOG_TAG, "afterTextChanged: minutes given is valid");
-            } else {
-                Log.d(LOG_TAG, "afterTextChanged: minutes given is invalid");
-            }
+        if(minutes.length() != 0){
+            int minute = Integer.valueOf(minutes.toString());
+            return minute <= 120 && minute >= 45;
+        } else {
+            return false;
         }
+
     }
 }
