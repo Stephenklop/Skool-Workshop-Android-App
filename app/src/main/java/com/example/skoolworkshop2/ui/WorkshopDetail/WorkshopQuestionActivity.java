120f19e5ed6b01f0df3f5c512ed1f000fe371b9e
Bas <bbuijsen@gmail.com>
2021-06-01 12:16:01 +0200

    made mail for workshop work

--- app/src/main/java/com/example/skoolworkshop2/ui/WorkshopDetail/WorkshopQuestionActivity.java
+++ app/src/main/java/com/example/skoolworkshop2/ui/WorkshopDetail/WorkshopQuestionActivity.java
@@ -2,12 +2,12 @@ package com.example.skoolworkshop2.ui.WorkshopDetail;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
+import android.text.Html;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
-import android.widget.Toast;
+import android.widget.RelativeLayout;
 
 import androidx.fragment.app.FragmentActivity;
 
@@ -21,6 +21,12 @@ public class WorkshopQuestionActivity extends FragmentActivity implements View.O
     private ImageButton mBackButton;
     private EditText mEmailEditText;
     private EditText mTelEditText;
+    private EditText mAmountOfPersonsEditText;
+    private EditText mDateEditText;
+    private EditText mTimeEditText;
+    private EditText mLocationEditText;
+    private EditText mCJPEditText;
+    private EditText mMessageEditText;
     private EmailValidator emailValidator = new EmailValidator();
     private TelValidator telValidator = new TelValidator();
 
@@ -34,16 +40,67 @@ public class WorkshopQuestionActivity extends FragmentActivity implements View.O
         mBackButton = findViewById(R.id.activity_workshop_question_btn_back);
         mEmailEditText = (EditText) findViewById(R.id.activity_workshop_question_et_email);
         mTelEditText = (EditText) findViewById(R.id.activity_workshop_question_et_phone);
+        mAmountOfPersonsEditText = (EditText) findViewById(R.id.activity_workshop_question_et_amount);
+        RelativeLayout rl = (RelativeLayout) findViewById(R.id.activity_workshop_question_et_date);
+        mDateEditText = (EditText) rl.findViewById(R.id.editText);
+        mTimeEditText = (EditText) findViewById(R.id.activity_workshop_question_et_time);
+        mLocationEditText = (EditText) findViewById(R.id.activity_workshop_question_et_location);
+        mCJPEditText = (EditText) findViewById(R.id.activity_workshop_question_et_cjp);
+        mMessageEditText = (EditText) findViewById(R.id.activity_workshop_question_et_message);
         // Set up validations
         mEmailEditText.addTextChangedListener(emailValidator);
         mTelEditText.addTextChangedListener(telValidator);
 
-        mSendBn.setText("Verzenden");
+
+
         mBackButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent backIntent = new Intent(getApplicationContext(), MainActivity.class);
-                startActivity(backIntent);
+                Intent intent = new Intent(getBaseContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        mSendBn.setText("Verzenden");
+        mSendBn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println(mAmountOfPersonsEditText.getText().toString());
+                System.out.println(mDateEditText.getText().toString());
+                System.out.println(mTimeEditText.getText().toString());
+                System.out.println(mLocationEditText.getText().toString());
+                System.out.println(mCJPEditText.getText().toString());
+                System.out.println(mEmailEditText.getText().toString());
+                System.out.println(mTelEditText.getText().toString());
+                System.out.println(mMessageEditText.getText().toString());
+                if(!mEmailEditText.getText().toString().isEmpty()){
+                    Intent emailIntent = new Intent(Intent.ACTION_SEND);
+                    emailIntent.setType("text/html");
+                    emailIntent.putExtra(Intent.EXTRA_EMAIL, new String[]{"info@skoolworkshop.nl"});
+                    emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Email vanuit de app");
+
+                    StringBuilder stringBuilder = new StringBuilder();
+                    stringBuilder.append("Contact informatie:");
+                    stringBuilder.append("\n-Email: " + mEmailEditText.getText().toString());
+                    stringBuilder.append("\n-TelefoonNummer: " + mTelEditText.getText().toString());
+                    stringBuilder.append("\nCJP schoolnummer: " + mCJPEditText.getText().toString());
+
+                    stringBuilder.append("\n\nWorkshop informatie:");
+                    stringBuilder.append("\n-Aantal personen: " + mAmountOfPersonsEditText.getText().toString());
+                    stringBuilder.append("\n-Gewenste datum: " + mDateEditText.getText().toString());
+                    stringBuilder.append("\n-Gewenste aanvangstijd: " + mTimeEditText.getText().toString());
+                    stringBuilder.append("\n-Gewenste locatie: " + mLocationEditText.getText().toString());
+
+                    stringBuilder.append("\n\nBericht:\n");
+                    stringBuilder.append(mMessageEditText.getText().toString());
+
+                    emailIntent.putExtra(Intent.EXTRA_TEXT, stringBuilder.toString());
+                    startActivity(emailIntent);
+
+                } else {
+                    System.out.println("Email is empty");
+                }
             }
         });
     }
