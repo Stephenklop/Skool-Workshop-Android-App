16637b4dd6cafc2711ac334df196cfe9c193df14
David van Mourik <dcwvanmourik@gmail.com>
2021-06-02 15:18:26 +0200

    Add shopping cart logic

--- app/src/main/java/com/example/skoolworkshop2/dao/LocalAppStorage.java
+++ app/src/main/java/com/example/skoolworkshop2/dao/LocalAppStorage.java
@@ -1,2 +1,41 @@
-package com.example.skoolworkshop2.dao;public class LocalAppStorage {
+package com.example.skoolworkshop2.dao;
+
+import android.content.Context;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import io.paperdb.Paper;
+
+public class LocalAppStorage {
+    private Context context;
+
+    public LocalAppStorage(Context context) {
+        this.context = context;
+        initDatabase();
+    }
+
+    private void initDatabase() {
+        Paper.init(context);
+    }
+
+    public void addToList(String key, Object value) {
+        List<Object> list = Paper.book().read(key, new ArrayList<>());
+        list.add(value);
+        Paper.book().write(key, list);
+    }
+
+    public void removeFromList(String key, int index) {
+        List<Object> list = Paper.book().read(key);
+        list.remove(index);
+        Paper.book().write(key, list);
+    }
+
+    public <T> List<T> getList(String key) {
+        return Paper.book().read(key);
+    }
+
+    public void deleteKey(String key) {
+        Paper.book().delete(key);
+    }
 }
---
+++ app/src/main/java/com/example/skoolworkshop2/ui/shoppingCart/ShoppingCartRecyclerViewAdapter.java
@@ -0,0 +1,103 @@
+package com.example.skoolworkshop2.ui.shoppingCart;
+
+import android.animation.AnimatorSet;
+import android.animation.ValueAnimator;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.skoolworkshop2.R;
+import com.example.skoolworkshop2.domain.Product;
+
+
+import java.util.List;
+
+public class ShoppingCartRecyclerViewAdapter extends RecyclerView.Adapter<ShoppingCartRecyclerViewAdapter.ViewHolder> {
+    List<Product> shoppingCartItems;
+    Context context;
+
+    public ShoppingCartRecyclerViewAdapter(List<Product> shoppingCartItems, Context context) {
+        this.shoppingCartItems = shoppingCartItems;
+        this.context = context;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_shopping_cart, parent, false);
+        ViewHolder holder = new ViewHolder(view);
+
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        holder.mWorkshopTitle.setText(shoppingCartItems.get(position).getName());
+        holder.mWorkshopPrice.setText("€ " + String.format("%.2f", shoppingCartItems.get(position).getPrice()).replace(".", ","));
+        holder.mDetailButton.setText("Details");
+        holder.mDetailButton.setOnClickListener(v -> {
+            if (holder.mDetailButton.getText().equals("Details")) {
+                holder.mDetailButton.setText("Verbergen");
+                animateDetails(holder.mDetails, 0, holder.height);
+            } else {
+                holder.mDetailButton.setText("Details");
+                animateDetails(holder.mDetails, holder.height,0);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return shoppingCartItems.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        // One line Layout
+        ImageView mWorkshopImage;
+        TextView mWorkshopTitle;
+        TextView mWorkshopPrice;
+        Button mDetailButton;
+        LinearLayout mDetails;
+        int height;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+
+            mWorkshopImage = itemView.findViewById(R.id.item_shopping_cart_iv_workshop);
+            mWorkshopTitle = itemView.findViewById(R.id.item_shopping_cart_tv_workshop);
+            mWorkshopPrice = itemView.findViewById(R.id.item_shopping_cart_tv_price);
+            mDetailButton = itemView.findViewById(R.id.item_shopping_cart_btn_details);
+            mDetails = itemView.findViewById(R.id.item_shopping_cart_ll_details);
+
+            mDetails.measure(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+            height = mDetails.getMeasuredHeight();
+            mDetails.getLayoutParams().height = 0;
+
+            mWorkshopImage.setClipToOutline(true);
+        }
+    }
+
+    private void animateDetails(View view, int currentValue, int newValue) {
+        ValueAnimator slideAnimator = ValueAnimator
+                .ofInt(currentValue, newValue)
+                .setDuration(300);
+
+        slideAnimator.addUpdateListener(animation -> {
+            Integer value = (Integer) animation.getAnimatedValue();
+            view.getLayoutParams().height = value.intValue();
+            view.requestLayout();
+        });
+
+        AnimatorSet set = new AnimatorSet();
+        set.play(slideAnimator);
+        set.setInterpolator(new AccelerateDecelerateInterpolator());
+        set.start();
+    }
+}
