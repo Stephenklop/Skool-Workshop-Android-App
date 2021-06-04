969f5c8acf99a96ed1d4c6b6a4e89bd37caf4819
FSGabrsek <f.gabrsek@gmail.com>
2021-06-04 12:14:01 +0200

    added activity_my_account with layout test activity

---
+++ app/src/main/java/com/example/skoolworkshop2/ui/MyAccountLayoutTestActivity.java
@@ -0,0 +1,16 @@
+package com.example.skoolworkshop2.ui;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.skoolworkshop2.R;
+
+public class MyAccountLayoutTestActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_account);
+
+    }
+}
