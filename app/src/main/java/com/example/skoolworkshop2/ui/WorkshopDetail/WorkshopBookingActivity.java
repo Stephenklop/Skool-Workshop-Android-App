41b595da3f75d58bd8a03e1caef2756ff450dcec
FSGabrsek <f.gabrsek@gmail.com>
2021-06-10 14:16:38 +0200

    added closing edittext touch event

--- app/src/main/java/com/example/skoolworkshop2/ui/WorkshopDetail/WorkshopBookingActivity.java
+++ app/src/main/java/com/example/skoolworkshop2/ui/WorkshopDetail/WorkshopBookingActivity.java
@@ -1,13 +1,17 @@
 package com.example.skoolworkshop2.ui.WorkshopDetail;
 
 import android.app.DatePickerDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Rect;
 import android.os.Build;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
@@ -353,5 +357,23 @@ public class WorkshopBookingActivity extends FragmentActivity implements View.On
         System.out.println("TOTALE PRIJS: " + workshop.getPrice());
         mTotalCostTextView.setText("Subtotaal: €" + (int) workshop.getPrice());
     }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            View v = getCurrentFocus();
+            if (v instanceof EditText) {
+                Rect outRect = new Rect();
+                v.getGlobalVisibleRect(outRect);
+                if (!outRect.contains((int) event.getRawX(), (int) event.getRawY())) {
+                    v.clearFocus();
+                    InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+                }
+            }
+        }
+        return super.dispatchTouchEvent(event);
+    }
+
 }
 
--- app/src/main/java/com/example/skoolworkshop2/ui/WorkshopDetail/WorkshopDetailActivity.java
+++ app/src/main/java/com/example/skoolworkshop2/ui/WorkshopDetail/WorkshopDetailActivity.java
@@ -81,7 +81,6 @@ public class WorkshopDetailActivity extends FragmentActivity implements View.OnC
             }
         });
 
-
         mPriceBn = findViewById(R.id.activity_workshop_detail_button_price);
         mParticipantsBn = findViewById(R.id.activity_workshop_detail_button_participants);
 //        mDurationBn = findViewById(R.id.activity_workshop_detail_button_duration);
