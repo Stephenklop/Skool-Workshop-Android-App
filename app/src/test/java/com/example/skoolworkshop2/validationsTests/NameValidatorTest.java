09fee009e82969ee81e1a9bfb4c761cbfc64e8bb
AzethS <fairys.ardhana@gmail.com>
2021-06-10 22:43:25 +0200

    NameValidator Testcases

--- app/src/test/java/com/example/skoolworkshop2/validationsTests/NameValidatorTest.java
+++ app/src/test/java/com/example/skoolworkshop2/validationsTests/NameValidatorTest.java
@@ -1,6 +1,43 @@
 package com.example.skoolworkshop2.validationsTests;
 
-public class NameValidatorTest {
+import com.example.skoolworkshop2.logic.validation.EmailValidator;
+import com.example.skoolworkshop2.logic.validation.addressInfoValidators.NameValidator;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class NameValidatorTest {
+    @Test
+    public void nameValidator_CorrectNameGiven_ReturnsTrue() {
+        assertTrue(NameValidator.isValidNameValidator("Pieter"));
+    }
+
+    @Test
+    public void nameValidator_CorrectNameGivenWithAllowedSymbol_ReturnsTrue() {
+        assertTrue(NameValidator.isValidNameValidator("O'Neal"));
+    }
+
+    @Test
+    public void nameValidator_CorrectNameGivenWithMultipleAllowedSymbol_ReturnsTrue() {
+        assertTrue(NameValidator.isValidNameValidator("O'Neal-o"));
+    }
+
+    @Test
+    public void nameValidator_IncorrectNameWrongSymbol_ReturnsFalse() {
+        assertFalse(NameValidator.isValidNameValidator("Ar@a"));
+    }
+
+    @Test
+    public void nameValidator_IncorrectNameEmpty_ReturnsFalse() {
+        assertFalse(NameValidator.isValidNameValidator(""));
+    }
+
+
+    @Test
+    public void nameValidator_NullName_ReturnsFalse() {
+        assertFalse(NameValidator.isValidNameValidator(null));
+    }
 
 }
