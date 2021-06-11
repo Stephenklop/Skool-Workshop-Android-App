abdf8f005dbe444d6a5be928689a145156a4644c
FSGabrsek <f.gabrsek@gmail.com>
2021-06-11 11:48:08 +0200

    removed point history from test activity

--- app/src/main/java/com/example/skoolworkshop2/ui/PointsLayoutTestActivity.java
+++ app/src/main/java/com/example/skoolworkshop2/ui/PointsLayoutTestActivity.java
@@ -19,20 +19,20 @@ public class PointsLayoutTestActivity extends AppCompatActivity {
 
 
 
-        for (int i = 14; i < 18; i++) {
-            TableLayout mTbl = findViewById(R.id.activity_points_tbl_point_history);
-            TableRow mRow = (TableRow) LayoutInflater.from(this)
-                    .inflate(R.layout.component_row_points, mTbl, false);
-
-            TextView mDate = mRow.findViewById(R.id.component_row_points_tv_date);
-            TextView mAction = mRow.findViewById(R.id.component_row_points_tv_action);
-            TextView mAmount = mRow.findViewById(R.id.component_row_points_tv_amount);
-
-            mDate.setText(i + " mei 2021");
-            mAction.setText("Registratie");
-            mAmount.setText("100");
-
-            mTbl.addView(mRow, new TableRow.LayoutParams());
-        }
+//        for (int i = 14; i < 18; i++) {
+//            TableLayout mTbl = findViewById(R.id.activity_points_tbl_point_history);
+//            TableRow mRow = (TableRow) LayoutInflater.from(this)
+//                    .inflate(R.layout.component_row_points, mTbl, false);
+//
+//            TextView mDate = mRow.findViewById(R.id.component_row_points_tv_date);
+//            TextView mAction = mRow.findViewById(R.id.component_row_points_tv_action);
+//            TextView mAmount = mRow.findViewById(R.id.component_row_points_tv_amount);
+//
+//            mDate.setText(i + " mei 2021");
+//            mAction.setText("Registratie");
+//            mAmount.setText("100");
+//
+//            mTbl.addView(mRow, new TableRow.LayoutParams());
+//        }
     }
 }
