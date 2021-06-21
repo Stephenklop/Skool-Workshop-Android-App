2f38621b2f4e5b37bc60b71e12158344a5f97d48
basbuijsen <bbuijsen@gmail.com>
2021-06-21 12:10:11 +0200

    finished SettingsActivity

--- app/src/main/java/com/example/skoolworkshop2/ui/SettingsActivity.java
+++ app/src/main/java/com/example/skoolworkshop2/ui/SettingsActivity.java
@@ -10,6 +10,8 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.skoolworkshop2.R;
+import com.example.skoolworkshop2.dao.localDatabase.LocalDb;
+import com.example.skoolworkshop2.dao.localDatabase.entities.SettingsEntity;
 import com.example.skoolworkshop2.logic.notifications.MessagingService;
 import com.google.firebase.analytics.FirebaseAnalytics;
 
@@ -29,9 +31,19 @@ public class SettingsActivity extends AppCompatActivity {
         mNotificationsSwitch = findViewById(R.id.activity_settings_tgl_notifications);
         mAnalyticsSwitch = findViewById(R.id.activity_settings_tgl_analytics);
 
+        if(LocalDb.getDatabase(getApplication()).getSettingsDAO().getSettings() == null){
+            LocalDb.getDatabase(getApplication()).getSettingsDAO().insertSettings(new SettingsEntity(1, true, true));
+        }
+
         mBackBtn.setOnClickListener(v -> {
             finish();
         });
+
+        boolean analyticsEnabled = LocalDb.getDatabase(getApplication()).getSettingsDAO().getSettings().isAnalytics();
+        boolean notificationsEnabled = LocalDb.getDatabase(getApplication()).getSettingsDAO().getSettings().isNotifications();
+
+        mNotificationsSwitch.setChecked(notificationsEnabled);
+        mAnalyticsSwitch.setChecked(analyticsEnabled);
 
         mNotificationsSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
@@ -40,8 +52,10 @@ public class SettingsActivity extends AppCompatActivity {
 
                 if(isChecked) {
                     messagingService.unsubscribeToTopic("main");
+                    LocalDb.getDatabase(getApplication()).getSettingsDAO().setNotificationsEnabled();
                 } else {
                     messagingService.subscribeToTopic("main");
+                    LocalDb.getDatabase(getApplication()).getSettingsDAO().setNotificationsDisabled();
                 }
             }
         });
@@ -53,8 +67,10 @@ public class SettingsActivity extends AppCompatActivity {
 
                 if(isChecked) {
                     messagingService.enableAnalytics(getApplicationContext());
+                    LocalDb.getDatabase(getApplication()).getSettingsDAO().setAnalyticsEnabled();
                 } else {
                     messagingService.disableAnalytics(getApplicationContext());
+                    LocalDb.getDatabase(getApplication()).getSettingsDAO().setAnalyticsDisabled();
                 }
             }
         });
